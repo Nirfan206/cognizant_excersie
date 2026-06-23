034bcb78dd41f38f9eea2d8ef182c17fcf7644f2
Nirfan206 <203888097+Nirfan206@users.noreply.github.com>
2026-06-23 18:57:08 +0530

    Add files via upload

---
+++ week1/FinancialForecasting/FinancialForecast.java
@@ -0,0 +1,38 @@
+public class FinancialForecast {
+
+    public static double predictFutureValue(double currentValue,
+                                            double growthRate,
+                                            int years) {
+
+        // Base Case
+        if (years == 0) {
+            return currentValue;
+        }
+
+        // Recursive Case
+        return predictFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1
+        );
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000.0;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double futureValue = predictFutureValue(
+                currentValue,
+                growthRate,
+                years
+        );
+
+        System.out.printf(
+                "Predicted Future Value after %d years = %.2f%n",
+                years,
+                futureValue
+        );
+    }
+}
