671bbddd0f3aececb75dd04cd1608146229c602f
Nirfan206 <203888097+Nirfan206@users.noreply.github.com>
2026-06-23 15:58:44 +0530

    Add files via upload

---
+++ week1/ Singleton_Pattern/SingletonPatternExample.java
@@ -0,0 +1,16 @@
+package javaFSE.week1.singleton;
+class Logger {
+    private static Logger instance;
+    private Logger() {
+        System.out.println("Logger instance created.");
+    }
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
